c077de97144286b4eb1bfe408f1ece43c96240b6
Sifat2245 <Sifat2245@users.noreply.github.com>
2025-05-21 00:36:27 +0600

    successfully connected with mongodb

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const {MongoClient, ServerApiVersion} = require('mongodb');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qwhtqkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+});
+
+
+const run = async() =>{
+    try{
+        await client.connect()
+
+        await client.db('admin').command({ping: 1})
+        console.log('connected');
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
 
 
 app.get('/', (req,res) =>{
@@ -14,3 +41,4 @@ app.get('/', (req,res) =>{
 app.listen(port, () =>{
     console.log(`the sever is running on port ${port}`);
 })
+
